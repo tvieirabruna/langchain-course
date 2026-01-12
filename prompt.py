baa40824189915e7574e2b141b39464ca29745c5
Bruna <brunavieira@nano.ufrj.br>
2026-01-12 16:59:22 -0300

    perf: refactor output parsing to use structured output

--- prompt.py
+++ prompt.py
@@ -12,7 +12,7 @@ Action Input: the input to the action
 Observation: the result of the action
 ... (this Thought/Action/Action Input/Observation can repeat N times)
 Thought: I now know the final answer
-Final Answer: the final answer to the original input question formatted accoding to format_instructions: {format_instructions}
+Final Answer: the final answer to the original input question
 
 Begin!
 
