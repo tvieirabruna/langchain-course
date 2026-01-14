8e5663517683c4a41df4044c63e99176d16f22ec
Bruna <brunavieira@nano.ufrj.br>
2026-01-14 13:48:16 -0300

    chore: first comment - implement RAG pipeline with and without LCEL for comparison

---
+++ ingestion.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+
+load_dotenv()
+
+if __name__ == "__main__":
+    print("Loading...")
+    loader = TextLoader("./mediumblog1.tx", encoding="utf-8")
+    document = loader.load()
+
+    print("Splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"Created {len(texts)} chunks.")
+
+    embeddings = OpenAIEmbeddings()
+
+    print("Ingesting...")
+    PineconeVectorStore.from_documents(
+        texts, embeddings, index_name=os.environ["INDEX_NAME"]
+    )
+    print("Finished.")
